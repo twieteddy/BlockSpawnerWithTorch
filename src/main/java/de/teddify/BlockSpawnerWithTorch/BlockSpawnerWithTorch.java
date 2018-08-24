aefefb3d35c019d48efa12c0690712f0f0e53be5
Richard <twieteddy@gmail.com>
2018-08-24 06:00:55 +0200

    Added mobspawner block event

--- src/main/java/de/teddify/BlockSpawnerWithTorch/BlockSpawnerWithTorch.java
+++ src/main/java/de/teddify/BlockSpawnerWithTorch/BlockSpawnerWithTorch.java
@@ -1,13 +1,25 @@
 package de.teddify.BlockSpawnerWithTorch;
 
+import org.bukkit.Material;
+import org.bukkit.block.BlockFace;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.SpawnerSpawnEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class BlockSpawnerWithTorch extends JavaPlugin {
+public class BlockSpawnerWithTorch extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new SpawnerSpawnEventListener(), this);
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {}
+
+    @EventHandler
+    public void onSpawnerSpawnEvent(SpawnerSpawnEvent e) {
+        if (e.getSpawner().getBlock().getRelative(BlockFace.UP, 1).getType() == Material.TORCH) {
+            e.setCancelled(true);
+        }
+    }
 }
